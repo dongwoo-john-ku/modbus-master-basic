461317f04f5164396b78ed455abef750ffafbe07
John Ku <dongwoo.john.ku@gmail.com>
2020-07-13 22:45:06 +0900

    upload - timer
    threading timer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pymodbus.client.sync import ModbusTcpClient
+import threading
 
-IP_ADDRESS = '192.168.1.100'
+IP_ADDRESS = '192.168.1.200'
 SERVER_PORT = 502
 
 def modbustcp_client(host = IP_ADDRESS, port = SERVER_PORT):
@@ -26,6 +27,34 @@ def read_input_regs(c):
         print("Modbus/TCP Connection is Not Connected\n")
         return None
 
+def write_output_regs(c, value):
+    if connect(c):
+        print("Modbus/TCP Connected\n")
+        print("Write Output Registers\n")
+        c.write_registers(0x4400,value)
+        # print(readInputRegisters.registers[0:100], "\n")
+        return True
+    else:
+        print("Modbus/TCP Connection is Not Connected\n")
+        return None
+
 def close(c):
     c.close()
     print("Modbus/TCP Connection is Closed\n")
+
+value = 0
+def startTimer():
+    global value
+    timer = threading.Timer(5, startTimer)
+    timer.start()
+    c = modbustcp_client(host=IP_ADDRESS, port=SERVER_PORT)
+
+    if value == 0 :
+        value = 1
+    else:
+        value = 0
+    write_output_regs(c, value)
+
+if __name__ == "__main__":
+    startTimer()
+
